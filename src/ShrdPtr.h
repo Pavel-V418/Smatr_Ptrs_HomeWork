89b3ccb983d714bdc9f6dc2f6625e76f1b514f5b
Павел <pasavild10@gmail.com>
2026-09-13 16:32:09 +0300

    create ShrdPrt add SubTyping

--- src/ShrdPtr.h
+++ src/ShrdPtr.h
@@ -11,6 +11,9 @@ private:
     Type* ptr;
     int* ptrs_counter;
 
+    template <class U>
+    friend class ShrdPtr;
+
     void clean(Type* p) {
         if constexpr (std::is_array<T>::value)
             delete[] p;
@@ -18,40 +21,117 @@ private:
             delete p;
     }
 
+    void counter_check_for_remove(Type* pointer,int* pointers_counter) {
+        if (pointers_counter && --(*pointers_counter) == 0) {
+            clean(pointer);
+            delete pointers_counter;
+        }
+    }
+
 public:
     explicit ShrdPtr(Type* p = nullptr)
-        : ptr(p) {
-        if (p != nullptr)
-            ptrs_counter = new int(1);
-        else
-            ptrs_counter = new int(0);
-        }
+        : ptr(p), ptrs_counter(new int(1)) {}
 
     ~ShrdPtr() {
-        if (--(*ptrs_counter) == 0) {
-            clean(ptr);
-            delete ptrs_counter;
-        }
+        counter_check_for_remove(ptr, ptrs_counter);
     }
 
     // присваивание и копирование
     ShrdPtr(const ShrdPtr& other) {
         if (this != &other) {
             ptr = other.ptr;
-            ptrs_counter++;
+            ptrs_counter = other.ptrs_counter;
+
+            if (ptrs_counter)
+                ++(*ptrs_counter);
         }
     }
 
     ShrdPtr& operator= (const ShrdPtr& other) {
         if (this != &other) {
+            counter_check_for_remove(ptr, ptrs_counter);
+
             ptr = other.ptr;
-            ptrs_counter == other.ptrs_counter;
-            ptrs_counter++;
+            ptrs_counter = other.ptrs_counter;
+
+            if (ptrs_counter)
+                ++(*ptrs_counter);
         }
         return *this;
     }
 
     // move-семантика
-    
+    ShrdPtr(ShrdPtr&& other) noexcept
+        : ptr(other.ptr), ptrs_counter(other.ptrs_counter) {
+        other.ptr = nullptr;
+        other.ptrs_counter = nullptr;
+    }
+
+    ShrdPtr& operator= (ShrdPtr&& other) noexcept {
+        if (this != &other) {
+            counter_check_for_remove(ptr, ptrs_counter);
+
+            ptr = other.ptr;
+            ptrs_counter = other.ptrs_counter;
+
+            other.ptr = nullptr;
+            other.ptrs_counter = nullptr;
+        }
+        return *this;
+    }
+
+    // Подтипизация
+    template <class U>
+    ShrdPtr(const ShrdPtr<U>& other)
+        : ptr(static_cast<Type*>(other.ptr)), ptrs_counter(other.ptrs_counter) {
+        if (ptrs_counter)
+            ++(*ptrs_counter);
+    }
+
+    template <class U>
+    ShrdPtr& operator= (const ShrdPtr<U>& other) {
+        counter_check_for_remove(ptr, ptrs_counter);
+
+        ptr = static_cast<Type*>(other.ptr);
+        ptrs_counter = other.ptrs_counter;
+
+        if (ptrs_counter)
+            ++(*ptrs_counter);
+
+        return *this;
+    }
+
+    template <class U>
+    ShrdPtr(ShrdPtr<U>&& other) noexcept
+        : ptr(static_cast<Type*>(other.ptr)), ptrs_counter(other.ptrs_counter) {
+        other.ptr = nullptr;
+        other.ptrs_counter = nullptr;
+    }
+
+    template <class U>
+    ShrdPtr& operator= (ShrdPtr<U>&& other) noexcept {
+        counter_check_for_remove(ptr, ptrs_counter);
+
+        ptr = static_cast<Type*>(other.ptr);
+        ptrs_counter = other.ptrs_counter;
+
+        other.ptr = nullptr;
+        other.ptrs_counter = nullptr;
+
+        return *this;
+    }
+
+    Type& operator*() const {return *ptr;}
+    Type* operator->() const {return ptr;}
+    Type& operator[](int index) const {return ptr[index];}
+
+    void reset(Type* p = nullptr) {
+        if (ptr != p) {
+            counter_check_for_remove(ptr, ptrs_counter);
+
+            ptr = p;
+            ptrs_counter = new int(1);
+        }
+    }
 
 };
